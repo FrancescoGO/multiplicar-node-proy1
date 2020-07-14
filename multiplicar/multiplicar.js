85e516e5df4adcba31dfb0dda2c3b5c760cc6340
FrancescoGO <francesco.go.os@gmail.com>
2020-07-13 20:24:12 -0500

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,57 @@
+// requireds
+const fs = require('fs');
+const colors = require('colors');
+
+let listarTabla = (base, limite) => {
+
+    return new Promise((resolve, reject) => {
+
+        if ( !Number(base) || !Number(limite) ) {
+            reject(`Uno de los valores introducidos no es un número: ${base} - ${limite}`);
+            return;
+        }
+
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+
+            data += (`${base} * ${i} = ${base*i}\n`);
+
+        }
+
+        return resolve(data);
+
+    });
+
+}
+
+let crearArchivo = (base, limite) => {
+
+    return new Promise((resolve, reject) => {
+
+        if ( !Number(base) || !Number(limite) ) {
+            reject(`Uno de los valores introducidos no es un número: ${base} - ${limite}`);
+            return;
+        }
+
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+
+            data += (`${base} * ${i} = ${base*i}\n`);
+
+        }
+
+        fs.writeFile(`tablas/tabla-${base}.txt`, data, (err) => {
+            if (err) reject(err);
+            resolve(`tabla-${base}.txt`);
+          });
+
+    });
+
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
